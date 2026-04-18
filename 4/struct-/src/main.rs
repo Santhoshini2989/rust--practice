f4b8d57e75a8ba9e4b27f873040653260e16e5a8
santhoshini <santhoshinia01@gmail.com>
2026-04-19 01:26:23 +0530

    structs

--- 4/struct-/src/main.rs
+++ 4/struct-/src/main.rs
@@ -1,1 +1,45 @@
- 
+ //blueprint -- defing a struct
+
+ struct User{
+    active:bool,
+    username:String,
+    email:String,
+    sign_in_count:u64,
+ }
+
+ //creating an instance
+ fn main(){
+    let user1 = User{
+       active:true,
+       username:String::from("pooja"),
+       email:String::from("abc@gmail.com"),
+       sigin_in_count:1,
+    };
+ }
+ //just add mut to be ab;le to change 
+ fn main(){
+    let mut  user1 = User{
+       active:true,
+       username:String::from("pooja"),
+       email:String::from("abc@gmail.com"),
+       sigin_in_count:1,
+    };
+    user1.email = String::from("xyz@gmail.com");
+ }
+ //update syntax 
+
+ //tuple structs 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main() {
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+//unit-like structs - - no fields  -- used when you wanna apply trait 
+
+struct AlwaysEqual;
+fn main(){
+    let subject = AlwaysEqual;
+}
