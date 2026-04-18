da9ad1da1574a43bd031a605f2209e636d438801
santhoshini <santhoshinia01@gmail.com>
2026-04-19 02:23:18 +0530

    sruct-example

---
+++ 4/struct-exammple/src/main.rs
@@ -0,0 +1,87 @@
+ //area of a rectangle
+ fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+//to make it more clear and readable -- here the area signature has 2 parameters which doent mention they are linked 
+//refactoring with tuple
+
+fn main() {
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+//using idnex is bad so we use structs
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+//Adding Functionality with Derived Traits - - just to print out 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {rect1:?}");
+}
+/* mostly for normal datatypes the println macro is linke to the display but here for the structs'
+its confused  so instead we use the debug trait {:#?} - prints all instead of {:?}  - 
+dbg! amcro -- thsi takes the ownership */
+
+//using dbg!
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+}
